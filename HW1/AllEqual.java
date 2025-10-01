e086d3f9c35b63f66a213df4b6654991c3e9f6a6
lazykamrii <phamquang2929@gmail.com>
2025-10-02 00:15:45 +0700

    a

---
+++ HW1/AllEqual.java
@@ -0,0 +1,5 @@
+public class AllEqual {
+    boolean isEqual(int a, int b, int c){
+        return a == b && b == c;
+    }
+}
---
+++ HW1/Introduction.java
@@ -0,0 +1,14 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Pham Van Vinh Quang\t24020287\tINT2204 6\tlazykamarii\t240200287@vnu.edu.vn");
+        for (int i = 9; i > 1; i--) {
+            System.out.println(i + " bottles of beer on the wall"
+                    + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+
+    }
+}
